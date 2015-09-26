ee7530093d2c88b44dc3bab3a97ac69d08d07357
Justin Maguire <maguirejustin@hotmail.com>
2015-09-26 13:27:34 -0400

    Removal of comment in Schema file to fix local db push to Heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20150821152849) do
 
-  # These are extensions that must be enabled in order to support this database
+
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
