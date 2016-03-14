5dcac44645b71c66d6cdbb81c798fa43e2c431a0
Justin Maguire <maguirejustin@hotmail.com>
2016-03-14 19:33:27 -0400

    Update to production.rb to fix send grid error after deploy to Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,21 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+
+
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'agile-crag-50266.herokuapp.com' }
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
