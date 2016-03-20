0f41500e0f2452ba4861596989cd9b94f8a97e82
Justin Maguire <maguirejustin@hotmail.com>
2016-03-20 10:26:14 -0400

    Heroku fix for uploading pics

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.aws_credentials = {
-    aws_access_key_id:   ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    access_key_id:   ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region:   'us-west-2'
   }
   config.aws_bucket = ENV['AWS_BUCKET']
